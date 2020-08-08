480deec8e6a0b39f8f6acdee155d7c537c6ebb23
Your Name <you@example.com>
2020-08-08 16:09:52 +0900

    first test.

--- aws_scheduler/__init__.py
+++ aws_scheduler/__init__.py
@@ -1,10 +1,12 @@
-import scheduler
 
-handler = scheduler.Scheduler()
+from .kernel import scheduler
 
 def deploy(template_dir="template/", 
-            delete_anther_schedule=False,
                 aws_access_key_id=None,
                 aws_secret_access_key=None,
-                region_name=None):
-    handler.deploy(template_dir, delete_anther_schedule, aws_access_key_id, aws_secret_access_key, region_name)
+                region_name=None,
+                no_cache=False,
+                delete_unmanaged=False):
+
+    handler = scheduler.Scheduler(template_dir, aws_access_key_id, aws_secret_access_key, region_name)
+    handler.deploy(no_cache=no_cache, delete_unmanaged=delete_unmanaged)
--- setup.py
+++ setup.py
@@ -5,21 +5,19 @@
 import io
 from setuptools import find_packages, setup
 
-setup(name='cloud_scheduler',
+setup(name='aws_scheduler',
       version='0.1',
       description='This package makes it easy to manage Glue Crawler and Cloudwatch schedulers on AWS services. You can schedule lambda function execution, etc.',
-      long_description="Please refer to the https://github.com/da-huin/cloud_scheduler",
+      long_description="Please refer to the https://github.com/da-huin/aws_scheduler",
       long_description_content_type="text/markdown",
-      url='https://github.com/da-huin/cloud_scheduler',
-      download_url= 'https://github.com/da-huin/cloud_scheduler/archive/master.zip',
+      url='https://github.com/da-huin/aws_scheduler',
+      download_url= 'https://github.com/da-huin/aws_scheduler/archive/master.zip',
       author='JunYeong Park',
       author_email='dahuin000@gmail.com',
       license='MIT',
       packages=find_packages(),
-      install_requires=[],
+      install_requires=["boto3", "template_manager"],
       classifiers=[
           'Programming Language :: Python :: 3',
     ]
 )
-
-
